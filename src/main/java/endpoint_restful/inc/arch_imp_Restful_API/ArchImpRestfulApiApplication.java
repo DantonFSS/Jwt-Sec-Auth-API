d14e5de47d8ba4cf5ae11a4d41422cae90e92a4f
Danton <researches.dev.pro@gmail.com>
2024-08-26 00:05:33 -0700

    Main Application

---
+++ src/main/java/endpoint_restful/inc/arch_imp_Restful_API/ArchImpRestfulApiApplication.java
@@ -0,0 +1,13 @@
+package endpoint_restful.inc.arch_imp_Restful_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchImpRestfulApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchImpRestfulApiApplication.class, args);
+	}
+
+}
